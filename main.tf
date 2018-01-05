0b0cd1a08c652e63f77fc9114eb916d8159698aa
Roger Berlind <roger@hashicorp.com>
2018-01-05 12:59:50 -0500

    added availability_zone to instance

--- main.tf
+++ main.tf
@@ -24,6 +24,7 @@ provider "aws" {
 resource "aws_instance" "ubuntu" {
   ami           = "${var.ami_id}"
   instance_type = "${var.instance_type}"
+  availability_zone = "${var.aws_region}-1a"
 
   tags {
     Name = "Provisioned-by-Terraform"
