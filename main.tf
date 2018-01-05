c2c070cc611add43f034cac2515772bb91bf7ebe
Roger Berlind <roger@hashicorp.com>
2018-01-05 12:03:17 -0500

    changed name of resource to ubuntu

--- main.tf
+++ main.tf
@@ -21,7 +21,7 @@ provider "aws" {
   region = "${var.aws_region}"
 }
 
-resource "aws_instance" "web" {
+resource "aws_instance" "ubuntu" {
   ami           = "${var.ami_id}"
   instance_type = "${var.instance_type}"
 }
