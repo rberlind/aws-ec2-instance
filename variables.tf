e62753394911d7ddb1668af6684dc68cdef84c0d
Roger Berlind <rberlind@optonline.net>
2018-01-11 09:50:58 -0500

    Back to east

--- variables.tf
+++ variables.tf
@@ -1,6 +1,6 @@
 variable "aws_region" {
   description = "AWS region"
-  default = "us-west-1"
+  default = "us-east-1"
 }
 
 variable "ami_id" {
@@ -10,7 +10,7 @@ variable "ami_id" {
 
 variable "instance_type" {
   description = "type of EC2 instance to provision."
-  default = "t2.small"
+  default = "t2.micro"
 }
 
 variable "name" {
