3a0f73d9e5821150a86868c758bcb08358a22097
Roger Berlind <rberlind@optonline.net>
2018-01-15 15:21:44 -0500

    added ec2 instance to module

--- module/main.tf
+++ module/main.tf
@@ -2,3 +2,13 @@ provider "aws" {
   alias = "hard-coded-in-module"
   region = "us-east-1"
 }
+
+resource "aws_instance" "ubuntu" {
+  ami           = "ami-2e1ef954"
+  instance_type = "t2.micro"
+  availability_zone = "us-east-1a"
+
+  tags {
+    Name = "from_module"
+  }
+}
