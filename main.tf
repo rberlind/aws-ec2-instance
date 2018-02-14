3050a001965f16261ee169204ffbed7abd2e2d82
Roger Berlind <rberlind@optonline.net>
2018-02-14 11:45:29 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,7 +8,6 @@ provider "aws" {
 resource "aws_instance" "ubuntu" {
   ami           = "${var.ami_id}"
   instance_type = "${var.instance_type}"
-  availability_zone = "${var.aws_region}a"
   key_name = "${var.key_name}"
   associate_public_ip_address = "true"
   root_block_device {
