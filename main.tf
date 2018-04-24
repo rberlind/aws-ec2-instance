e8f179d3d5dd1d00dd7aaaa004a25231d6663b54
Roger Berlind <roger@hashicorp.com>
2018-04-24 16:32:00 -0400

    get AWS keys from Vault

--- main.tf
+++ main.tf
@@ -1,9 +1,29 @@
 terraform {
-  required_version = ">= 0.11.1"
+  required_version = ">= 0.11.7"
+}
+# Set VAULT_TOKEN environment variable
+provider "vault" {
+  address = "${var.vault_addr}"
 }
 
+# AWS credentials from Vault
+data "vault_aws_access_credentials" "aws_creds" {
+  backend = "aws-tf"
+  role = "deploy"
+}
+
+# Insert 15 second delay so AWS credentials definitely available
+# at all AWS endpoints
+data "external" "region" {
+  # Delay so that new keys are available across AWS
+  program = ["./delay-vault-aws", "${var.aws_region}"]
+}
+
+#  Setup the AWS provider
 provider "aws" {
-  region = "${var.aws_region}"
+  access_key = "${data.vault_aws_access_credentials.aws_creds.access_key}"
+  secret_key = "${data.vault_aws_access_credentials.aws_creds.secret_key}"
+  region  = "${data.external.region.result["region"]}"
 }
 
 resource "aws_instance" "ubuntu" {
