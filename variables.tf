0b6082b9ad5b0aa51babdb60c4358b2de9835b7d
Guy Barros <38210175+GuyBarros@users.noreply.github.com>
2018-06-21 12:24:10 +0100

    change region

--- variables.tf
+++ variables.tf
@@ -1,11 +1,11 @@
 variable "aws_region" {
   description = "AWS region"
-  default = "us-east-1"
+  default = "eu-west-2"
 }
 
 variable "ami_id" {
   description = "ID of the AMI to provision. Default is Ubuntu 14.04 Base Image"
-  default = "ami-2e1ef954"
+  default = "ami-524e5736"
 }
 
 variable "instance_type" {
