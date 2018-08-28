edfea016607d35157c54ed9a202779c37f5cbc89
Roger Berlind <roger@hashicorp.com>
2018-08-28 12:11:43 -0400

    removed second instance

--- main.tf
+++ main.tf
@@ -39,17 +39,3 @@ resource "aws_instance" "ubuntu" {
     Name = "${var.name}"
   }
 }
-
-resource "aws_instance" "ubuntu2" {
-  ami           = "${var.ami_id}"
-  instance_type = "${var.instance_type}"
-  availability_zone = "${var.aws_region}a"
-  key_name = "${var.key_name}"
-  associate_public_ip_address = "true"
-  root_block_device {
-    volume_size = "${var.volume_size}"
-  }
-  tags {
-    Name = "${var.name}-2"
-  }
-}
