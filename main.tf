fce530abaf5e14812f9d81e2f651ac5f41648a37
Roger Berlind <roger@hashicorp.com>
2019-04-20 13:14:50 -0400

    count to 2

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "ubuntu" {
-  count         = "1"
+  count         = "2"
   ami           = "${var.ami_id}"
   instance_type = "${var.instance_type}"
   availability_zone = "${var.aws_region}b"
