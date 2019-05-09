e88a3945ae1d29278dd5997f8e1e1effbb21252f
Roger Berlind <roger@hashicorp.com>
2019-05-09 10:43:00 -0400

    update outputs for ubuntu-too

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
 output "public_dns" {
-  value = "${aws_instance.ubuntu.public_dns}"
+  value = "${aws_instance.ubuntu.*.public_dns}"
+}
+
+output "public_dns_too" {
+  value = "${aws_instance.ubuntu-too.*.public_dns}"
 }
