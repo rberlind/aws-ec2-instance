096017ca28139952de7537bdc4fabe7228e67d70
Roger Berlind <roger@hashicorp.com>
2019-06-14 10:21:46 -0400

    test branch protection with m5.large

--- variables.tf
+++ variables.tf
@@ -10,7 +10,8 @@ variable "ami_id" {
 
 variable "instance_type" {
   description = "type of EC2 instance to provision."
-  default = "t2.micro"
+  #default = "t2.micro"
+  default = "m5.large"
 }
 
 variable "name" {
