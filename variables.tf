8e70f9c1416419bafedfc599caed56782bac0835
Roger Berlind <roger@hashicorp.com>
2019-06-14 10:26:32 -0400

    back to t2.micro

--- variables.tf
+++ variables.tf
@@ -10,8 +10,7 @@ variable "ami_id" {
 
 variable "instance_type" {
   description = "type of EC2 instance to provision."
-  #default = "t2.micro"
-  default = "m5.large"
+  default = "t2.micro"
 }
 
 variable "name" {
