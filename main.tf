11e823ce74777102d6f285714f6eb39dccab994f
Roger Berlind <roger@hashicorp.com>
2019-09-17 14:01:45 -0400

    add rbd volume_size

--- main.tf
+++ main.tf
@@ -11,6 +11,9 @@ resource "aws_instance" "ubuntu" {
   instance_type = "${var.instance_type}"
   availability_zone = "${var.aws_region}b"
   associate_public_ip_address = "true"
+  root_block_device {
+    volume_size = 10
+  }
   tags {
     Name = "${var.name}"
     owner = "roger@hashicorp.com"
