1f25d134d79c69c7d4295405155bdc9c88713dfc
rberlind <roger@hashicorp.com>
2020-04-14 09:10:36 -0400

    add module dir

---
+++ module/main.tf
@@ -0,0 +1,16 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+resource "aws_instance" "ubuntu" {
+  # count         = 3
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  availability_zone = format("%sb", var.aws_region)
+  associate_public_ip_address = var.associate_public_ip_address
+
+  tags = {
+    Name = var.name
+    Owner = "rberlind"
+  }
+}
---
+++ module/variables.tf
@@ -0,0 +1,23 @@
+variable "aws_region" {
+  description = "AWS region"
+  default = "us-east-1"
+}
+
+variable "ami_id" {
+  description = "ID of the AMI to provision. Default is Ubuntu 14.04 Base Image"
+  default = "ami-2e1ef954"
+}
+
+variable "instance_type" {
+  description = "type of EC2 instance to provision."
+  default = "t2.micro"
+}
+
+variable "name" {
+  description = "name to pass to Name tag"
+  default = "roger-demo-nested"
+}
+
+variable "associate_public_ip_address" {
+  default = true
+}
